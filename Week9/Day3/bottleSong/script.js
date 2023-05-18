8d580b856a2c0cb08b4e101082dac018ae01c86a
bloomy2000 <Milesbloom526@gmail.com>
2023-05-18 12:27:14 +0300

    push4 d2s8

--- Week9/Day3/bottleSong/script.js
+++ Week9/Day3/bottleSong/script.js
@@ -19,8 +19,13 @@ bottlesChange = bottlesStart
 console.log(bottlesStart)
 console.log(bottlesChange)
 for (let i = 1; i <= bottlesStart; i++) {
-    console.log("We have " + bottlesChange + " bottles of beer on the wall");
-    if (i == 1) {
+    if (bottlesChange === 1) {
+        console.log("We have 1 bottle of beer on the wall");
+    } else {
+        console.log("We have " + bottlesChange + " bottle of beer on the wall");
+    }
+    
+    if (i === 1) {
         console.log("Take _1_ down, pass it around");
     } else {
         console.log("Take _" + i + "_ down, pass them around");
@@ -29,7 +34,9 @@ for (let i = 1; i <= bottlesStart; i++) {
     if (bottlesChange === 0){
         console.log("We now have no bottles");
         break
+    } else if (bottlesChange === 1) {
+        console.log("We now have 1 bottle");
     } else {
-        console.log("We now have " + bottlesChange + " bottles");
+        console.log("We have " + bottlesChange + " bottle of beer on the wall");
     }
 }
